e9b9ff12ac2a2ebf302fc8285c04f5bae24b1630
David Snider <david@davidsnider.org>
2020-11-08 12:53:56 -0700

    don't build with gh actions

--- docker/icinga-server/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Setup some defaults before launching Icinga
-
-icinga2 api setup --cn '*.thesniderpad.com'
-icinga2 feature enable ido-pgsql
-
-exec "/usr/sbin/icinga2 --no-stack-rlimit daemon -e /var/log/icinga2/icinga2.err -x notice"
